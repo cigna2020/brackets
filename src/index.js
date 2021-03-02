6d114ec286031597fb0d62cfd2d74a338af43bc0
Alex <cigan@ua.fm>
2021-03-02 13:33:18 +0200

    feat: add a solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsObj = {};
+    bracketsConfig.forEach(el => {
+        bracketsObj[el[0]] = el[1];
+    });
+
+    const res = [];
+    const newStr = str.split('')
+
+    for (const elem of newStr) {
+        if (res[0] == elem) {
+            res.shift();
+        } else if (bracketsObj[elem]) {
+            res.unshift(bracketsObj[elem]);
+        } else {
+            return false;
+        }
+    }
+
+    return res.length === 0;
 }
